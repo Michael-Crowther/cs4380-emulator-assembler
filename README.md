390dde0b37848e8dfc66ce3aa8bad421f29fde5a
Michael-Crowther <md.crowther@yahoo.com>
2024-02-04 11:34:09 -0700

    Finished project 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Hello, this is my README
+Project 1 Emulator - Mark 1 of the 4380
+
+This emulator accepts up to two command line arguments for reading in a binary file with an optional memory size. It then enters an execution loop to fetch, decode, and execute valid instructions specified by the ISA. I've written tests for these functions, which can be found in the src folder, and run with ./runTests after compiling Cmake and make. 
---
+++ include/emu4380.h
@@ -0,0 +1,33 @@
+#ifndef EMU_4380_H
+#define EMU_4380_H
+#include<array>
+
+constexpr size_t num_gen_regs = 22;
+constexpr size_t num_cntrl_regs = 5;
+constexpr size_t num_data_regs = 2;
+
+enum class RegNames{
+	R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, PC, SL, SB, SP, FP, HP = 21
+};
+
+enum class CntrlRegNames{
+	OPERATION=0, OPERAND_1, OPERAND_2, OPERAND_3, IMMEDIATE
+};
+
+enum class DataRegNames{
+	REG_VAL_1, REG_VAL_2
+};
+
+extern unsigned int reg_file[num_gen_regs];
+extern unsigned char* prog_mem;
+extern unsigned int cntrl_regs[num_cntrl_regs];
+extern unsigned int data_regs[num_data_regs];
+extern unsigned int memorySize;
+
+bool fetch();
+
+bool decode();
+
+bool execute();
+
+#endif
