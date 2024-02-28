544fa9a54be56d22e5ea151fc0f4fe5deafc0de5
Michael-Crowther <md.crowther@yahoo.com>
2024-02-27 21:02:27 -0700

    fixing emulator

---
+++ src/CMakeFiles/emu4380.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emu4380.dir/emu4380.cpp.o"
+  "CMakeFiles/emu4380.dir/emu4380.cpp.o.d"
+  "CMakeFiles/emu4380.dir/main.cpp.o"
+  "CMakeFiles/emu4380.dir/main.cpp.o.d"
+  "emu4380"
+  "emu4380.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/emu4380.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
