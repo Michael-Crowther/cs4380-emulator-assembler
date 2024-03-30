5c1e342e6d7a71f26558c9d04306c443544d7ed0
Michael-Crowther <md.crowther@yahoo.com>
2024-03-30 13:39:27 -0600

    broken

--- src/emu4380.cpp
+++ src/emu4380.cpp
@@ -160,11 +160,6 @@ bool decode(){
   unsigned int operand2 = cntrl_regs[OPERAND_2];
   unsigned int operand3 = cntrl_regs[OPERAND_3];
 
-	cout << "operation: " << operation << endl;
-	cout << "operand1: " << operand1 << endl;
-	cout << "operand2: " << operand2 << endl;
-	cout << "operand3: " << operand3 << endl;
-
 	//check validity of operation
 	if(validOperations.find(operation) == validOperations.end()){
 		return false;
@@ -213,9 +208,12 @@ bool execute(){
 	unsigned int operand2 = cntrl_regs[OPERAND_2];
 	unsigned int operand3 = cntrl_regs[OPERAND_3];
 	unsigned int immediate = cntrl_regs[IMMEDIATE];
-	//cout << "execute" << endl;
-	//cout << "operation: " << operation << endl;
-	//cout << data_regs[R3] << endl;
+
+	cout << "operation: " << operation << endl;
+	cout << "operand2: " << operand2 << endl;
+	cout << "operand3: " << operand3 << endl;
+	cout << "immediate: " << immediate << endl;
+	cout << "data_regs[R3]: " << data_regs[R3] << endl;
 
 	switch(operation){
 		case 1: //JMP
@@ -236,7 +234,7 @@ bool execute(){
 			break;
 		case 11: //LDR
 			if(immediate >= memorySize) return false;
-			cout << data_regs[R3] << endl;
+			//cout << data_regs[R3] << endl;
 			reg_file[data_regs[REG_VAL_1]] = globalCache->readWord(immediate);
 			break;
 		case 12: //STB
